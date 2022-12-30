1317322f660f8c4da243fb9156a92e932ad7630a
Gian Baccin <112916868+Gian-Baccin@users.noreply.github.com>
2022-12-30 13:50:07 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,11 +5,14 @@ function encode(){
     let texto = textInput.value;
     let text = texto.toLowerCase();
 
+
     let resultEncode = text.replace(/e/g, "enter").replace(/i/g, "imes")
     .replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
 
     document.getElementById('output').innerHTML = '<textarea readonly id="input">' + resultEncode + 
     '</textarea>' + '<div class="container-btn"><button class="btn-copy" id="copy" onclick="copy()">Copy</button></div>'
+    
+    window.scrollBy(0,500);
     
 }
 
@@ -22,12 +25,19 @@ function decode(){
     
     document.getElementById('output').innerHTML = '<textarea readonly id="input">' + resultDecode + 
     '</textarea>' + '<div class="container-btn"><button class="btn-copy" id="copy" onclick="copy()">Copy</button></div>'
+
+    window.scrollBy(0,500);
+
 }
 
 function copy(){
     let textCopy = document.getElementById('input');
 
+    textInput.select();
+    document.execCommand('delete');
+
     textCopy.select();
     document.execCommand('copy');
     // alert("The message has been successfully copied!")
 }
+
